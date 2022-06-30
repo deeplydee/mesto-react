dfd71b8fc571cbe3d9688315f2e9e102506d5f3a
deeplydee <dmitry.pletyukhin@gmail.com>
2022-06-30 13:04:19 +0300

    add first components

---
+++ src/components/Main.js
@@ -0,0 +1,44 @@
+import avatar from '../images/avatar.jpg';
+
+function Main() {
+  return (
+    <main className="content">
+      <section className="profile">
+        <div className="profile__avatar-container">
+          <img
+            className="profile__avatar"
+            src={avatar}
+            alt="Аватар пользователя"
+          />
+          <button
+            className="profile__avatar-button"
+            type="button"
+            aria-label="Изменить аватар"
+          ></button>
+        </div>
+        <div className="profile__info">
+          <div className="profile__wrapper">
+            <h1 className="profile__name">Жак-Ив Кусто</h1>
+            <button
+              className="profile__edit-button"
+              type="button"
+              aria-label="Редактировать профиль"
+            ></button>
+          </div>
+          <p className="profile__description">Исследователь океана</p>
+        </div>
+        <button
+          className="profile__add-button"
+          type="button"
+          aria-label="Добавить карточку"
+        ></button>
+      </section>
+
+      <section className="photo-grid">
+        <ul className="photo-grid__list"></ul>
+      </section>
+    </main>
+  )
+}
+
+export default Main;
